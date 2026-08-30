775edc615aa8784488ba301a24fa8eccc577dbba
shannonhubbell <brundlefly+github@gmail.com>
2026-08-30 16:48:29 -0700

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://shannonhubbell.github.io/',
+
   vite: {
     plugins: []
-  }
+  },
+
+  integrations: [sitemap()]
 });
